37a75bf5dfa74fa33c14b04bf3e45fc99593068d
Kolesnikov Vladimir <lodoss.kolesnikov@gmail.com>
2018-12-25 12:05:44 +0300

    Added promise and dymamic added all block

---
+++ js/dynamic.js
@@ -0,0 +1,50 @@
+const article = (i, text, urlImage) => `
+  <div class="article${i}">
+  <div class="article${i}__header container-row">
+      <div class="article${i}__square"></div>
+      <div class="article${i}__headline">ABOUT SUPER LOGO</div>
+      <div class="article${i}__rectangle"></div>
+  </div>
+  <div class="article${i}__body container-row">
+      <div class="article${i}_image"><img src="${urlImage}">--></div>
+      <div class="article${i}__text-block container-column">
+          <div class="article${i}__text">
+            <p>${text}</p>
+          </div>
+          <div class="article${i}__footer">
+              <span><a>Read more...</a></span>
+          </div>
+      </div>
+  </div>
+  </div>
+`;
+$(document).ready(function() {
+    $("#baconButton").click(function() {
+
+    Promise.all([httpGetData('GET', 'https://baconipsum.com/api/?callback=?type=all-meat&start-with-lorem=0&paras=3&sentence=0'), httpGetData('GET', 'https://picsum.photos/400/400/?random')])
+        .then((results) => {
+            $('#article').prepend(article(1, results[0].responseText, results[1].responseURL));
+        })
+        .catch((err) => {
+            console.log(err);
+          });
+});
+});
+
+  function httpGetData (method, url) {
+    return new Promise((resolve, reject) => {
+      var xhr = new XMLHttpRequest();
+      xhr.open(method, url);
+      xhr.onload = function () {
+        if (this.status === 200) {
+          resolve(xhr);
+        } else {
+          reject();
+        }
+      };
+      xhr.onerror = () => {
+        reject();
+      };
+      xhr.send();
+    });
+  }
